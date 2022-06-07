4f8fec48eb31f595698d8c0e06db26e08029e464
Kwirke <nebrios2@gmail.com>
2022-06-07 23:01:46 +0200

    Add some tests

---
+++ src/Tests/NotesListTest.elm
@@ -0,0 +1,70 @@
+module Tests.NotesListTest exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import NotesList
+import OpaqueDict
+import Test exposing (..)
+
+
+intDictFrom =
+    OpaqueDict.fromList String.fromInt
+
+
+suite : Test
+suite =
+    describe "NotesList"
+        [ describe "move"
+            [ test "should move an element from one dictionary to an empty one" <|
+                \_ ->
+                    let
+                        dictA =
+                            intDictFrom [ ( 4, "a" ), ( 2, "b" ), ( 42, "x" ) ]
+
+                        dictB =
+                            intDictFrom []
+
+                        expectedA =
+                            intDictFrom [ ( 4, "a" ), ( 42, "x" ) ]
+
+                        expectedB =
+                            intDictFrom [ ( 2, "b" ) ]
+
+                        actual =
+                            NotesList.move 2 dictA dictB
+                    in
+                    actual |> Expect.equal ( expectedA, expectedB )
+            , test "should move an element to a non-empty dictionary" <|
+                \_ ->
+                    let
+                        dictA =
+                            intDictFrom [ ( 4, "a" ), ( 2, "b" ), ( 42, "x" ) ]
+
+                        dictB =
+                            intDictFrom [ ( 3, "e" ), ( 5, "f" ) ]
+
+                        expectedA =
+                            intDictFrom [ ( 4, "a" ), ( 2, "b" ) ]
+
+                        expectedB =
+                            intDictFrom [ ( 3, "e" ), ( 5, "f" ), ( 42, "x" ) ]
+
+                        actual =
+                            NotesList.move 42 dictA dictB
+                    in
+                    actual |> Expect.equal ( expectedA, expectedB )
+            , test "should do nothing if element is not in origin" <|
+                \_ ->
+                    let
+                        dictA =
+                            intDictFrom []
+
+                        dictB =
+                            intDictFrom [ ( 2, "a" ) ]
+
+                        actual =
+                            NotesList.move 42 dictA dictB
+                    in
+                    actual |> Expect.equal ( dictA, dictB )
+            ]
+        ]
