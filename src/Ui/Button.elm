3a4bc42d9df062290981825f9302dc9162ae1028
Kwirke <nebrios2@gmail.com>
2022-06-09 00:04:42 +0200

    Style Button states and improve code

--- src/Ui/Button.elm
+++ src/Ui/Button.elm
@@ -12,22 +12,6 @@ import Html.Styled.Attributes exposing (css)
 import Html.Styled.Events exposing (onClick)
 
 
-neutral =
-    neutral300
-
-
-neutralLight =
-    neutral100
-
-
-accentGreenLight =
-    accentGreen100
-
-
-accentGreen =
-    accentGreen500
-
-
 buttonStyles : List Style
 buttonStyles =
     [ displayFlex
@@ -35,24 +19,30 @@ buttonStyles =
     , padding2 (px 10) (px 20)
     , justifyContent center
     , fontSize (rem 1.2)
-    , color (hex neutral)
+    , color (hex neutral300)
     , backgroundColor transparent
-    , border3 (px 2) solid (hex neutralLight)
+    , border3 (px 2) solid (hex neutral100)
     , borderRadius (px 10)
-    , property "box-shadow"
-        ("inset 0 0 10px #"
-            ++ neutral
-            ++ ", 0 0 10px #"
-            ++ neutral
-        )
+    , property "box-shadow" (shadow 10 neutral300 ++ ", " ++ insetShadow 10 neutral300)
     , cursor pointer
     , buttonTransition 200
     , hover
-        [ borderColor (hex accentGreenLight)
-        , color (hex accentGreenLight)
-        , property "text-shadow" greenBlueBlurGradient
-        , property "box-shadow" (greenBlueBlurGradient ++ ", " ++ greenBlueInsetBlurGradient)
+        [ borderColor (hex accentGreen100)
+        , color (hex accentGreen100)
+        , property "text-shadow" greenBlueShadow
+        , property "box-shadow" greenBlueInsetShadow
         , buttonTransition 50
+        , focus [ outline3 (px 3) solid (hex accentGreen100) ]
+        ]
+    , active
+        [ borderColor (hex white)
+        , color (hex white)
+        , property "text-shadow" whiteShadow
+        , property "box-shadow" whiteInsetShadow
+        , focus [ outline none ]
+        ]
+    , focus
+        [ outline3 (px 3) solid (hex neutral300)
         ]
     ]
 
@@ -66,22 +56,49 @@ buttonTransition duration =
         ]
 
 
-greenBlueBlurGradient =
-    "0 0 10px #"
-        ++ accentGreen
-        ++ ", 0 0 15px #"
-        ++ accentBlue650
-        ++ ", 0 0 20px #"
-        ++ accentBlue650
+shadow : Int -> String -> String
+shadow size color =
+    "0 0 " ++ String.fromInt size ++ "px #" ++ color
 
 
-greenBlueInsetBlurGradient =
-    "inset 0 0 10px #"
-        ++ accentGreen
-        ++ ", inset 0 0 15px #"
-        ++ accentBlue650
-        ++ ", inset 0 0 20px #"
-        ++ accentBlue650
+insetShadow : Int -> String -> String
+insetShadow size color =
+    "inset " ++ shadow size color
+
+
+greenBlueShadow =
+    [ shadow 10 accentGreen500
+    , shadow 15 accentBlue650
+    , shadow 20 accentBlue650
+    ]
+        |> String.join ", "
+
+
+greenBlueInsetShadow =
+    [ shadow 10 accentGreen500
+    , shadow 15 accentBlue650
+    , shadow 20 accentBlue650
+    , insetShadow 10 accentGreen500
+    , insetShadow 15 accentBlue650
+    , insetShadow 20 accentBlue650
+    ]
+        |> String.join ", "
+
+
+whiteShadow =
+    [ shadow 10 backgroundPurple100
+    , shadow 15 accentBlue150
+    ]
+        |> String.join ", "
+
+
+whiteInsetShadow =
+    [ shadow 10 backgroundPurple100
+    , shadow 15 accentBlue150
+    , insetShadow 10 backgroundPurple100
+    , insetShadow 15 accentBlue150
+    ]
+        |> String.join ", "
 
 
 button : { label : String, onUse : msg, customStyle : List Style } -> Html msg
