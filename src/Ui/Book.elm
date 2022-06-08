ac8f3c800082a9372317c1624f2ea2dc98fd14fb
Kwirke <nebrios2@gmail.com>
2022-06-08 22:52:42 +0200

    Make book use dark theme by default

--- src/Ui/Book.elm
+++ src/Ui/Book.elm
@@ -1,12 +1,16 @@
 module Ui.Book exposing (main)
 
-import ElmBook exposing (withChapters)
+import ElmBook exposing (withChapters, withThemeOptions)
 import ElmBook.ElmCSS exposing (Book, Chapter, book)
+import ElmBook.ThemeOptions
 import Ui.Button
 
 
 main =
     book "Components"
+        |> withThemeOptions
+            [ ElmBook.ThemeOptions.preferDarkMode
+            ]
         |> withChapters
             [ Ui.Button.docs
             ]
