c4298435a549541cc27edb3087ddc54b4e05e753
Kwirke <nebrios2@gmail.com>
2022-06-08 22:52:08 +0200

    Add basic palette shades

---
+++ src/DesignSystem/Colors.elm
@@ -0,0 +1,205 @@
+module DesignSystem.Colors exposing (..)
+
+
+accentGreen900 =
+    "001a0f"
+
+
+accentGreen850 =
+    "00341e"
+
+
+accentGreen800 =
+    "004e2e"
+
+
+accentGreen750 =
+    "00683d"
+
+
+accentGreen700 =
+    "00824c"
+
+
+accentGreen650 =
+    "009c5b"
+
+
+accentGreen600 =
+    "00b66a"
+
+
+accentGreen550 =
+    "00d17a"
+
+
+accentGreen500 =
+    "00e886"
+
+
+accentGreen450 =
+    "0aff99"
+
+
+accentGreen400 =
+    "29ffa6"
+
+
+accentGreen350 =
+    "47ffb3"
+
+
+accentGreen300 =
+    "66ffbf"
+
+
+accentGreen250 =
+    "85ffcc"
+
+
+accentGreen200 =
+    "a3ffd9"
+
+
+accentGreen150 =
+    "c2ffe6"
+
+
+accentGreen100 =
+    "e0fff2"
+
+
+accentBlue900 =
+    "071514"
+
+
+accentBlue850 =
+    "0e2a27"
+
+
+accentBlue800 =
+    "143f3b"
+
+
+accentBlue750 =
+    "1b544e"
+
+
+accentBlue700 =
+    "226962"
+
+
+accentBlue650 =
+    "297e75"
+
+
+accentBlue600 =
+    "309389"
+
+
+accentBlue550 =
+    "36a89c"
+
+
+accentBlue500 =
+    "3ebeb2"
+
+
+accentBlue450 =
+    "50c6bb"
+
+
+accentBlue400 =
+    "66cdc3"
+
+
+accentBlue350 =
+    "7cd5cc"
+
+
+accentBlue300 =
+    "92dcd4"
+
+
+accentBlue250 =
+    "a8e3dd"
+
+
+accentBlue200 =
+    "beeae5"
+
+
+accentBlue150 =
+    "d3f1ee"
+
+
+accentBlue100 =
+    "e9f8f6"
+
+
+neutral900 =
+    "190c00"
+
+
+neutral850 =
+    "321900"
+
+
+neutral800 =
+    "4b2500"
+
+
+neutral750 =
+    "643200"
+
+
+neutral700 =
+    "7d3e00"
+
+
+neutral650 =
+    "964b00"
+
+
+neutral600 =
+    "af5700"
+
+
+neutral550 =
+    "c76400"
+
+
+neutral500 =
+    "e07000"
+
+
+neutral450 =
+    "ff8001"
+
+
+neutral400 =
+    "ff9021"
+
+
+neutral350 =
+    "ffa041"
+
+
+neutral300 =
+    "ffb060"
+
+
+neutral250 =
+    "ffc080"
+
+
+neutral200 =
+    "ffcfa0"
+
+
+neutral150 =
+    "ffdfc0"
+
+
+neutral100 =
+    "ffefdf"
